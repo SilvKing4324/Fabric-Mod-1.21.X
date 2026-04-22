e71bf830f6f98d19de51d94676fe5523c7468281
silvking4324 <SilvKing432@gmail.com>
2026-04-22 16:30:57 +0200

    Finalized Version 1.1.0

--- src/main/java/net/silvking432/silvkingsmod/dimension/ModDimensions.java
+++ src/main/java/net/silvking432/silvkingsmod/dimension/ModDimensions.java
@@ -8,11 +8,9 @@ import net.minecraft.world.dimension.DimensionType;
 import net.silvking432.silvkingsmod.SilvKingsMod;
 
 public class ModDimensions {
-    // Der Key für die Dimension selbst (Level/Welt)
     public static final RegistryKey<World> DARK_WORLD_KEY = RegistryKey.of(RegistryKeys.WORLD,
             Identifier.of("silvkingsmod", "dark_world"));
 
-    // Der Key für den Dimension-Type (die physikalischen Regeln aus deiner JSON)
     public static final RegistryKey<DimensionType> DARK_WORLD_TYPE_KEY = RegistryKey.of(RegistryKeys.DIMENSION_TYPE,
             Identifier.of("silvkingsmod", "dark_world_type"));
 
